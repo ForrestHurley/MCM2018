facab8a66523e8be27f13a4e1aba9b16517e1ec0
Karna Morey <Karna_Morey@Karnas-MacBook-Pro.local>
2018-02-08 20:55:01 -0500

    New file that deals with reflection, initial setup, but no substantial code written yet.

---
+++ atmosphere.py
@@ -0,0 +1,6 @@
+#inputs the input vector and point of origin, and outputs the new direction vector
+#normal and direction are numpy arrays of length 3
+#point is  numpy array of length 3 as well, except it refers to the point at which the ray had last been reflected
+def reflect_ray(normal, point, direction):
+
+
