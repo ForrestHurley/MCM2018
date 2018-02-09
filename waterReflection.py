54ce9efd7da482dd7f11d2a5be16105650289427
ForrestHurley <forrestelihurley0@gmail.com>
2018-02-08 21:01:42 -0500

    Added waterreflection class to calculate new normals after water interactions

---
+++ waterReflection.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class waterReflection:
+    def __init__(self):
+        self.reflect_function=mirror
+        self.waterparams=(,)
+
+    def calc_reflection(self,normals,rays)
+        return self.reflect_function(normals,rays,*self.waterparams)
