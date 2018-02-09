38a9d3c834bb58f6120b7565729d50d8a7862433
Karna Morey <Karna_Morey@Karnas-MacBook-Pro.local>
2018-02-09 10:27:20 -0500

    Finished debugging the atmosphere file for the spherical reflections. Wrote some data regarding a simple test case to output.csv, and will now work on generating visualizations.

--- atmosphere.py
+++ atmosphere.py
@@ -5,7 +5,8 @@ import numpy as np
 #normal and direction are numpy arrays of length 3
 #point is  numpy array of length 3 as well, except it refers to the point at which the ray had last been reflected
 def mirror(vectors,normals):
-    return np.subtract(vectors, 2*np.sum(vectors*normals,axis=1)*normals)
+    reflect= np.subtract(vectors, 2*np.sum(vectors*normals,axis=1)*normals)
+    return reflect
 
 def points_of_incidence(directions, line_points, normal_vector,surface_point):
     difference=np.subtract(surface_point, line_points)
@@ -13,16 +14,68 @@ def points_of_incidence(directions, line_points, normal_vector,surface_point):
     dot2=np.sum(directions*normal_vector, axis=1)
     return  np.add(line_points, (dot1/dot2)*directions)
     
-def points_of_incidence(center, radius, line_points, directions):
+def points_of_incidence2(center, radius, line_points, directions):
+    directions=unitize(directions)
     origin_difference=np.subtract(line_points, center)
-    term1=-2*(np.sum(directions*origin_difference, axis=1)
-    term2=term1**2-4*np.linalg.norm(line_points,ord=1)
+    a=np.linalg.norm(directions,axis=1)**2
+    b=(np.sum(directions*origin_difference, axis=1))
+    c=np.linalg.norm(origin_difference, axis=1)**2-radius**2
+    distance1=(-b+(b**2-a*c)**0.5)
+    distance2=(-b-(b**2-a*c)**0.5)
+    return (np.add(line_points,distance1*directions),np.add(line_points,distance2*directions))
+
+def normal_sphere(points_of_incidence):
+    return unitize(points_of_incidence)
+    
+
+def unitize(vectors):
+    return np.divide(vectors,np.linalg.norm(vectors,axis=1))
+
+def distances(vectors, other_vectors):
+    return np.linalg.norm(np.subtract(vectors, other_vectors),axis=1)
+
+def reflect_rays_sphere(center, radius, points, directions):
+    incidence_points=points_of_incidence2(center,radius, points, directions)
+    boolean_table=np.greater(distances(incidence_points[0],points),distances(incidence_points[1],points))
+    incidence_points=np.where(boolean_table, incidence_points[1],incidence_points[0])            
+    normals=normal_sphere(incidence_points)
+    new_vectors=unitize(mirror(directions, normals))
+    return(incidence_points, new_vectors)
+    
 def reflect_rays(normals, points, directions, surface_point, surface_normal):
     incidence_points=points_of_incidence(directions,points, surface_normal, surface_point)
     new_vectors=mirror(directions, normals)
     return (incidence_points, new_vectors)
 
 def main():
+    points=np.array([[200,0,0]])
+    center=np.array([0,0,0])
+     
+    directions=unitize(np.array([[1,1,0]]))
+
+
+    radius=200
+    radius2=220
+    
+    out=np.empty((40,3))
+    for i in range(20):
+        new_stuff=reflect_rays_sphere(center,radius2,points, directions)
+        points=new_stuff[0]
+        directions=new_stuff[1]
+
+        out[2*i]=points[0]
+        
+        new_stuff=reflect_rays_sphere(center,radius,points, directions)
+
+        points=new_stuff[0]
+        directions=new_stuff[1]
+       
+        
+        out[2*i+1]=points[0]
+
+    print(out)
+    np.savetxt('output.csv', out, delimiter=',')
+def imain():
     surface_point=np.array([0,0,0])
     surface_normal=np.array([0,1,0])
    
